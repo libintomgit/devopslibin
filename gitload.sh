78bd74c0043be0f6149e139c93e9075519410a70
libintomgit <libintomkk@gmail.com>
2022-04-03 20:42:39 +0530

    testing the commit from shell script :by libin

--- gitload.sh
+++ gitload.sh
@@ -10,7 +10,10 @@ read mesg
 
 sudo git status
 sudo git add .
-sudo git commit -m "$mesg"
+echo "added the changes to commit"
+sudo git commit -m "$mesg :by libin"
+echo "commited the changes with the message: "$mesg""
 sudo git push origin
+echo "pushed the changes to the remote server"
 
-
+git status
