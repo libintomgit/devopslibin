0e508ef2631605342de419c799d9a0897b620173
libintomgit <libintomkk@gmail.com>
2022-04-03 20:44:33 +0530

    updated the gitloadshell :by libin

--- gitload.sh
+++ gitload.sh
@@ -15,5 +15,6 @@ sudo git commit -m "$mesg :by libin"
 echo "commited the changes with the message: "$mesg""
 sudo git push origin
 echo "pushed the changes to the remote server"
-
+\n
+echo "re-checking the status"
 git status
