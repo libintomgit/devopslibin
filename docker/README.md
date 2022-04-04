a9fe43c24e9c0dfe33532cfa9b58635fd189973f
libintomgit <libintomkk@gmail.com>
2022-04-04 17:16:59 +0530

    docker update

---
+++ docker/README.md
@@ -0,0 +1,9 @@
+Lesson1 : Intro to docker
+Lesson2: Docker fundamentals
+Lesson3: Getting started with docker
+Lesson4: Build, Manage and distribute images
+Lesson5: Container configuration
+Lesson6: Networking
+Lesson7: Orchastration
+Lesson8: Container storage and volumes
+Lesson9: Security
--- gitload.sh
+++ gitload.sh
@@ -1,20 +1,37 @@
 #!/usr/bin/env bash
-#
+#this script is run the git command quick
 #
 #
 #commi_detail=$1
 #com_mesg= read "Enter commit meessage"
 
-echo "Enter commit message"
-read mesg
+#echo "Enter commit message"
+#read mesg
+CHANGES=`git status | grep "Changes not staged for commit"`
+echo $CHANGES
+if [[CHANGES -eq "Changes not staged for commit" ]]; then	
+	sudo git add .
+	echo "added the changes to commit"
+	echo "Enter commit message"
+	read mesg
+	sudo git commit -m "$mesg :by libin"
+	echo "commited the changes with the message: "$mesg""
+	sudo git push origin
+	echo "pushed the changes to the remote server"
+	echo "re-checking the status"
+	git status
+else
+	echo "Nothing to commit - Aborting the script"
+fi
 
-sudo git status
-sudo git add .
-echo "added the changes to commit"
-sudo git commit -m "$mesg :by libin"
-echo "commited the changes with the message: "$mesg""
-sudo git push origin
-echo "pushed the changes to the remote server"
-\n
-echo "re-checking the status"
-git status
+
+#sudo git status
+#sudo git add .
+#echo "added the changes to commit"
+#sudo git commit -m "$mesg :by libin"
+#echo "commited the changes with the message: "$mesg""
+#sudo git push origin
+#echo "pushed the changes to the remote server"
+#\n
+#echo "re-checking the status"
+#git status
